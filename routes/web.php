dfdc26dd0127d8c03bec099d4456b0bc8c2f5be6
pringgojs <odyinggo@gmail.com>
2019-10-17 07:58:19 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/vtweb', 'PagesController@vtweb');
+
+Route::get('/vtdirect', 'PagesController@vtdirect');
+Route::post('/vtdirect', 'PagesController@checkout_process');
+
+Route::get('/vt_transaction', 'PagesController@transaction');
+Route::post('/vt_transaction', 'PagesController@transaction_process');
+
+Route::post('/vt_notif', 'PagesController@notification');
+
+Route::get('/gopay', 'SnapController@gopay');
+Route::get('/snap', 'SnapController@snap');
+Route::get('/snaptoken', 'SnapController@token');
+Route::post('/snapfinish', 'SnapController@finish');
+
+Route::get('/', function () {
+    return view('welcome');
+});
